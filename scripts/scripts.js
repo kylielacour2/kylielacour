f2229279a3d352603be47f2ed6d7b26abe4335cd
Kylie LaCour <kylielacour@gmail.com>
2018-08-21 12:38:29 -0700

    hi

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,30 +1,30 @@
-$(document).ready(function() {
-    
+ $(document).ready(function() {
+
     console.log("ready");
 
     offset = 0;
-    
+
     $(window).on('scroll', function(event) {
 
         scrolltop = $(window).scrollLeft();
-        
+
         scrolltop = Math.round(scrolltop / 20);
 
         image_num = scrolltop%13;
         console.log(image_num);
-        
 
-        $('.piece').css('background-image', 'url("work/' + image_num + '.jpg")');
+
+        //$('.piece').css('background-image', 'url("work/' + image_num + '.jpg")');
 
     });
-    
+
     $(window).scroll(function() {
         var scrolledY = $(window).scrollLeft();
-        $('.PIECE').css('background-position', 'top ' + ((scrolledY)) + 'px');
+        //$('.PIECE').css('background-position', 'top ' + ((scrolledY)) + 'px');
     });
-    
+
     $('.fine').removeClass('fine--hover', 'fine--clicked');
-    
+
     if( !/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
         $('.fine').mouseenter(function(){
             $('.fine').addClass('fine--hover');
@@ -34,9 +34,9 @@ $(document).ready(function() {
             $('.fine').removeClass('fine--hover');
         })
     }
-    
 
-    
+
+
     $('.fine').click(function(e){
         e.preventDefault;
         $('.fine').toggleClass('fine--clicked');
@@ -44,5 +44,5 @@ $(document).ready(function() {
             window.location.href = 'http://www.wearefine.com';
         }, 400);
     })
-    
+
 });
