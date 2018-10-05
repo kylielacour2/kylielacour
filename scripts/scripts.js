0260d792278dcd2aedbcaa4b88283f6769a91691
Kylie LaCour <kylielacour@gmail.com>
2018-10-05 12:04:36 -0700

    hi

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -45,4 +45,20 @@
         }, 400);
     })
 
+    flexFont = function () {
+        var divs = document.getElementsByClassName("flexFont");
+        for(var i = 0; i < divs.length; i++) {
+            var relFontsize = divs[i].offsetWidth*0.05;
+            divs[i].style.fontSize = relFontsize+'px';
+        }
+    };
+
+    window.onload = function(event) {
+        flexFont();
+    };
+    window.onresize = function(event) {
+        flexFont();
+    };
+
+
 });
