69fa77e13d20234498bfc90ef3de45304359c3e5
Kylie LaCour <kylielacour@gmail.com>
2018-10-05 12:35:46 -0700

    hi

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -45,19 +45,30 @@
         }, 400);
     })
 
-    flexFont = function () {
-        var divs = document.getElementsByClassName("flexFont");
-        for(var i = 0; i < divs.length; i++) {
-            var relFontsize = divs[i].offsetHeight*0.05;
-            divs[i].style.fontSize = relFontsize+'px';
-        }
-    };
-
-    window.onload = function(event) {
-        flexFont();
-    };
-    window.onresize = function(event) {
-        flexFont();
+    $.fn.autoSizr = function () {
+      var el, elements, _i, _len, _results;
+      elements = $(this);
+      if (elements.length < 0) {
+        return;
+      }
+      _results = [];
+      for (_i = 0, _len = elements.length; _i < _len; _i++) {
+        el = elements[_i];
+        _results.push((function(el) {
+          var resizeText, _results1;
+          resizeText = function() {
+            var elNewFontSize;
+            elNewFontSize = (parseInt($(el).css('font-size').slice(0, -2)) - 1) + 'px';
+            return $(el).css('font-size', elNewFontSize);
+          };
+          _results1 = [];
+          while (el.scrollHeight > el.offsetHeight) {
+            _results1.push(resizeText());
+          }
+          return _results1;
+        })(el));
+      }
+      return $(this);
     };
 
 
